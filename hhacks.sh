6d08def4b304cf334932acf58b63039b561fc66c
hwac121 <hwac121@protonmail.com>
2019-06-20 18:13:01 -0400

    Handy Utilities
    This comes in handy after getting into that network you been hitting.

---
+++ hhacks.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+
+# Title: Handy Hacks
+# Version: 1.0
+# Author: hwac121
+# eMail: hwac121@protonmail.com
+
+# URL: http://www.majikcat.com
+# GitHub: 
+# Description:
+# Just a handy collection of commands put together in this utility
+# script I put together.
+
+#-----------------------------------------------------------------------
+
+#=======================================================================
+#                         VARIABLES
+#=======================================================================
+
+
+
+#=======================================================================
+#                         FUNCTIONS
+#=======================================================================
+
+Splash(){
+	echo -e " "
+	echo -e "\e[30m	 ██░ ██ ▄▄▄      ███▄    █▓█████▓██   ██▓    ██░ ██ ▄▄▄      ▄████▄  ██ ▄█▀ ██████ \e[0m";
+	sleep 0.08
+	echo -e "\e[30m	▓██░ ██▒████▄    ██ ▀█   █▒██▀ ██▒██  ██▒   ▓██░ ██▒████▄   ▒██▀ ▀█  ██▄█▒▒██    ▒ \e[0m";
+	sleep 0.08
+	echo -e "\e[30m	▒██▀▀██▒██  ▀█▄ ▓██  ▀█ ██░██   █▌▒██ ██░   ▒██▀▀██▒██  ▀█▄ ▒▓█    ▄▓███▄░░ ▓██▄   \e[0m";
+	sleep 0.08
+	echo -e "\e[30m	░▓█ ░██░██▄▄▄▄██▓██▒  ▐▌██░▓█▄   ▌░ ▐██▓░   ░▓█ ░██░██▄▄▄▄██▒▓▓▄ ▄██▓██ █▄  ▒   ██▒\e[0m";
+	sleep 0.08
+	echo -e "\e[30m	░▓█▒░██▓▓█   ▓██▒██░   ▓██░▒████▓ ░ ██▒▓░   ░▓█▒░██▓▓█   ▓██▒ ▓███▀ ▒██▒ █▒██████▒▒\e[0m";
+	sleep 0.08
+	echo -e "\e[30m	 ▒ ░░▒░▒▒▒   ▓▒█░ ▒░   ▒ ▒ ▒▒▓  ▒  ██▒▒▒     ▒ ░░▒░▒▒▒   ▓▒█░ ░▒ ▒  ▒ ▒▒ ▓▒ ▒▓▒ ▒ ░\e[0m";
+	sleep 0.08
+	echo -e "\e[30m	 ▒ ░▒░ ░ ▒   ▒▒ ░ ░░   ░ ▒░░ ▒  ▒▓██ ░▒░     ▒ ░▒░ ░ ▒   ▒▒ ░ ░  ▒  ░ ░▒ ▒░ ░▒  ░ ░\e[0m";
+	sleep 0.08
+	echo -e "\e[30m	 ░  ░░ ░ ░   ▒     ░   ░ ░ ░ ░  ░▒ ▒ ░░      ░  ░░ ░ ░   ▒  ░       ░ ░░ ░░  ░  ░  \e[0m";
+	sleep 0.08
+	echo -e "\e[30m	 ░  ░  ░     ░  ░        ░   ░   ░ ░         ░  ░  ░     ░  ░ ░     ░  ░        ░  \e[0m";
+	sleep 0.08
+	echo -e "\e[30m	                           ░     ░ ░                        ░                      \e[0m";
+	sleep 0.08
+	echo " "
+	echo -e "    	                            ====== Version 1.0 ======"
+}
+
+CreditSplash(){
+echo -e "\e[31m		                            ....+((zzz((-....\e[0m"
+sleep 0.2
+echo -e "\e[31m		                      ..+6llllllllllllllllllllZz+.\e[0m"
+sleep 0.2
+echo -e "\e[31m		                  .-OllllllltllllllltllllllllllllllOi.\e[0m"
+sleep 0.2
+echo -e "\e[31m		               .JOllllllllllllltllllltltlltlllllllllllOI.\e[0m"
+sleep 0.2
+echo -e "\e[31m		            ..6lllllltlltlOt?=!~         !?7<OOllllllllllwi.\e[0m"
+sleep 0.2
+echo -e "\e[31m		          .(OlllllllllOv7                       ?1ylltlllllO6.\e[0m"
+sleep 0.2
+echo -e "\e[31m		         .OlllllllltO=                             ?COlllllllOn.\e[0m"
+sleep 0.2
+echo -e "\e[31m		       .Sllllllltlln.                                 7ylllllllv+\e[0m"
+sleep 0.2
+echo -e "\e[31m		      .Olllltlllllllvi                                  ?slltllltn\e[0m"
+sleep 0.2
+echo -e "\e[31m		     Jllltlllltlltllllvi                                  TOllllllG.\e[0m"
+sleep 0.2
+echo -e "\e[31m		    Jlllllltu= TOlllllllZ<            .                  c .sltllllw.\e[0m"
+sleep 0.2
+echo -e "\e[31m		   .lltllltv    .4OlllllllZ<          (                  b  .kllltllG\e[0m"
+sleep 0.2
+echo -e "\e[31m		  .OlllltOr       .1OlltllllZ........ M                  M    SllllllL\e[0m"
+sleep 0.2
+echo -e "\e[31m		  Dllllltr          .1Olltllll1TMMMMM.Mx.      ..........M    .ylltlld.\e[0m"
+sleep 0.2
+echo -e "\e[31m		 .llltllJ             .2llllllllidMMMN.YMMMMMMMMMMMMMMMM       .OlltllL\e[0m"
+sleep 0.2
+echo -e "\e[31m		 Klllllu\           .JMMmvltlllllliTMMMN&x MMMMMMMMMM           Sllllld\e[0m"
+sleep 0.2
+echo -e "\e[31m		.6ltllld           .MMMMMMmvtlltlllliTMMM] HMMMMMMMM]           .lltllt]\e[0m"
+sleep 0.2
+echo -e "\e[31m		.llltll]          .MMMMMMMMMmzlllllllliTM] .MMMMMMMM            .llllll]\e[0m"
+sleep 0.2
+echo -e "\e[31m		.llllll]      x  .MMMMMMMMMMMMm1lltlllllzb  (MMMMMM]            .lltlll]\e[0m"
+sleep 0.2
+echo -e "\e[31m		.ltlltl]      b  MMMMMMMMMMMMMMMm1llllllllI. 4MMMM#             .llltll]\e[0m"
+sleep 0.2
+echo -e "\e[31m		.ylllllG      M .MMMMMMMMMMMMMMMMMm1lltlllll6.TMMD              .lllllt]\e[0m"
+sleep 0.2
+echo -e "\e[31m		 Kllltld      M  MMMMMMMMMMMMMMMMMMMg1llltlllt6.                Dtlllld\e[0m"
+sleep 0.2
+echo -e "\e[31m		 jllllll]     (MgMMMMMMMMMMMMMMMMMMMMMg1llllllll6.             .llltll2\e[0m"
+sleep 0.2
+echo -e "\e[31m		 .kltlltd.      .!!MMMMMMMMMMF????????TMe1lltllllt6.          .Olllltd^\e[0m"
+sleep 0.2
+echo -e "\e[31m		  .lllllld.          MMMMMMMMM         MM]?ylltllllO6.       .Iltllllr\e[0m"
+sleep 0.2
+echo -e "\e[31m		   4lltlllw.           .!""MMM         MM]  7ylltllllOn.    .OlllltlJ\e[0m"
+sleep 0.2
+echo -e "\e[31m		    Slllllld.     .MMa.    MMM         MM]    7slllllllOn..Ollllllld!\e[0m"
+sleep 0.2
+echo -e "\e[31m		     4ltllllOn   (MMMMM    ?MM         TM]      7yltlllllllllltlltv!\e[0m"
+sleep 0.2
+echo -e "\e[31m		      4tlltlllZ< .WMM#^      ?                    7ylllllllltllltv\e[0m"
+sleep 0.2
+echo -e "\e[31m		       ?slllllllZ<.                                 4lllltlllllu=\e[0m"
+sleep 0.2
+echo -e "\e[31m		         TOlltllllOI.                             .vlltlllltlOv!\e[0m"
+sleep 0.2
+echo -e "\e[31m		          .1OllllllllOI..                      .JOllllltllltv'\e[0m"
+sleep 0.2
+echo -e "\e[31m		             TylltlltllllOz<...          ...zIlllllltllllOv'\e[0m"
+sleep 0.2
+echo -e "\e[31m		               ?1OlllllllllllllltOOOOOtlltllllllllllllty=!\e[0m"
+sleep 0.2
+echo -e "\e[31m		                  ?1ylltlllllllllllllllllltlllltltlOv7!\e[0m"
+sleep 0.2
+echo -e "\e[31m		                      ?7lOlltlllllllltlllllltlOy?7!\e[0m"
+sleep 0.2
+echo -e "\e[31m		                             ?7<<llllzz<+7=?~\e[0m"
+sleep 0.2
+echo -e " "
+echo -e "\e[34m               =================================================================\e[0m"
+echo -e " "
+}
+#=======================================================================
+#                      CHECK FOR ROOT
+#=======================================================================
+
+#if [ "$EUID" -ne 0 ]
+#  then 
+#	clear
+#	splash
+#	echo "\e[31mPlease run as root or sudo ./snatch or sudo snatch\e[0m"
+#	sleep 0.5
+#  exit
+#fi
+
+#=======================================================================
+#                     MAIN MENU SYSTEM
+#=======================================================================
+
+options=("Dictionary" "Office Tools" "External IP" "Internal IP" "Full IP Info" "Run Traceroute" "Running Processes" "All Stats" "System-Wide Stats" "Zenmap Discovery" "WebDir Your Choice" "tail a FILE" "Credits" "Quit")
+PS3='Choose what action to take: '
+while [ "$menu" != 1 ]; do
+	clear
+	Splash
+	echo -e " "
+	sleep 0.25
+	select opt in "${options[@]}"; do
+    	case $opt in 
+
+#=======================================================================
+#                       MAIN PROGRAM
+#=======================================================================
+
+		"Dictionary")
+			clear
+			Splash
+			echo -e " "
+			echo -e "What word shall I look up for you?"
+			read lookup
+			look $lookup
+			echo " "
+			echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 5 seconds...\e[0m"
+			sleep 5
+		break
+		;;
+
+		"Office Tools")
+			clear
+			Splash
+			./officetools.sh
+		break
+		;;
+#=======================================================================
+#                          IP INFORMATION
+#=======================================================================
+
+		"External IP")
+			clear
+			Splash
+			echo " "
+			echo -e "External IP:"
+			curl ifconfig.me
+			echo " "
+			echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 5 seconds...\e[0m"
+			sleep 5
+		break
+		;;
+
+		"Internal IP")
+			clear
+			Splash
+			echo " "
+			echo -e "Internal IP:"
+			ifconfig
+			echo " "
+			echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 10 seconds...\e[0m"
+			sleep 10
+		break
+		;;
+
+		"Full IP Info")
+		clear
+		Splash
+		echo -e " "
+		echo -e "Your full IP information is:"
+		curl ipinfo.io
+		echo " "
+		echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 8 seconds...\e[0m"
+		sleep 8
+		break
+		;;
+
+		"Run Traceroute")
+			clear
+			Splash
+			echo " "
+			echo -e "What is the domain or IP you wish to run a traceroute on? (ie. www.domain.com or 192.168.1.1)"
+			read SearchTarget
+			mtr $SearchTarget
+			echo " "
+			echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 5 seconds...\e[0m"
+			sleep 5
+		break
+		;;
+
+#=======================================================================
+#                       ALL SYSTEM STATS READ
+#=======================================================================
+
+		"Running Processes")
+			clear
+			Splash
+			echo -e " "
+			echo -e "NOTE: Use Ctrl+c and close popup window to continue..."
+			echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 4 seconds...\e[0m"
+			ps
+			sleep 3
+		break
+		;;
+		
+		"All Stats")
+			clear
+			Splash
+			echo -e " "
+			echo -e "NOTE: Hit Ctrl+c and close popup to continue..."
+			xterm -hold -e dstat -cdngy
+		break
+		;;
+		
+		"System-Wide Stats")
+			clear
+			Splash
+			echo -e " "
+			echo -e "\e[31mNOTE:\e[0m \e[7mUse F10 to close the popup and continue.\e[0m"
+			htop
+		break
+		;;
+		
+		"Zenmap Discovery")
+			zenmap
+		break
+		;;
+#=======================================================================
+#                       HTTP VIEW AND TAIL VIEW
+#=======================================================================
+		
+		"WebDir Your Choice")
+		clear
+		Splash
+		echo -e " "
+		echo -e "NOTE: Hit Ctrl+c to stop the http server"
+		echo -e "What ip should this serve from? (ie. 192.168.0.23)"
+		read serveip
+		echo -e "What port should this serve from? (ie. 8080)"
+		read serveport
+		echo -e "What directory will be acting as the WebDir root? (ie. /path/to/directory)"
+		read servedir
+		python3 -m http.server -b $serveip -d $servedir $serveport
+		break
+		;;
+
+		"tail a FILE")
+		clear
+		Splash
+		echo -e " "
+		echo -e "What is the name or path of the file you wish too view live?" 
+		echo -e "(ie. myfile.log or /my/path/myfile.txt)"
+		echo " "
+		echo -e "NOTE: Use Ctrl+c and then close the popup to continue."
+		read myfile
+		xterm -hold -e tail -f $myfile
+		break
+		;;
+		
+#=======================================================================
+#                              CREDITS
+#=======================================================================
+
+		"Credits")
+		clear
+		Splash
+		sleep 0.15
+		CreditSplash
+		echo -e " "
+		echo -e "Handy Hacks"
+		sleep 0.15
+		echo -e "Version 1.0 - Written June 2019"
+		sleep 0.15
+		echo -e "Developed by hwac121"
+		echo " "
+		sleep 0.15
+		echo -e "URL: www.majikcat.com"
+		sleep 0.15
+		echo -e "GitHub: https://www.github.com/hwac121"
+		echo " "
+		sleep 0.15
+		echo -e "\e[31mNOTE:\e[0m \e[7mYou will return to the main menu in 6 seconds...\e[0m"
+		sleep 6
+		break
+		;;
+
+#=======================================================================
+#                               QUIT
+#=======================================================================
+
+		"Quit")
+			clear
+			Splash
+			echo " "
+			echo -e "                                Thanks for using Handy Hacks v1.0"
+			sleep 1.5
+			menu=1
+			clear
+		break
+		;;
+	* )
+			echo -e "$REPLY is an invalid option"
+		break
+		;;
+    	esac
+	done
+done
+
+exit 0
---
+++ setup.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Title: Handy Hacks
+# Version: 1.0
+# Author: hwac121
+# eMail: hwac121@protonmail.com
+
+# URL: http://www.majikcat.com
+# GitHub: 
+# Description:
+# Just a handy collection of commands put together in this utility
+# script I put together.
+
+#=======================================================================
+#                         FUNCTIONS
+#=======================================================================
+
+Splash(){
+	echo -e " "
+	echo -e "\e[32m	 ██░ ██ ▄▄▄      ███▄    █▓█████▓██   ██▓    ██░ ██ ▄▄▄      ▄████▄  ██ ▄█▀ ██████ \e[0m";
+	sleep 0.08
+	echo -e "\e[32m	▓██░ ██▒████▄    ██ ▀█   █▒██▀ ██▒██  ██▒   ▓██░ ██▒████▄   ▒██▀ ▀█  ██▄█▒▒██    ▒ \e[0m";
+	sleep 0.08
+	echo -e "\e[32m	▒██▀▀██▒██  ▀█▄ ▓██  ▀█ ██░██   █▌▒██ ██░   ▒██▀▀██▒██  ▀█▄ ▒▓█    ▄▓███▄░░ ▓██▄   \e[0m";
+	sleep 0.08
+	echo -e "\e[32m	░▓█ ░██░██▄▄▄▄██▓██▒  ▐▌██░▓█▄   ▌░ ▐██▓░   ░▓█ ░██░██▄▄▄▄██▒▓▓▄ ▄██▓██ █▄  ▒   ██▒\e[0m";
+	sleep 0.08
+	echo -e "\e[32m	░▓█▒░██▓▓█   ▓██▒██░   ▓██░▒████▓ ░ ██▒▓░   ░▓█▒░██▓▓█   ▓██▒ ▓███▀ ▒██▒ █▒██████▒▒\e[0m";
+	sleep 0.08
+	echo -e "\e[32m	 ▒ ░░▒░▒▒▒   ▓▒█░ ▒░   ▒ ▒ ▒▒▓  ▒  ██▒▒▒     ▒ ░░▒░▒▒▒   ▓▒█░ ░▒ ▒  ▒ ▒▒ ▓▒ ▒▓▒ ▒ ░\e[0m";
+	sleep 0.08
+	echo -e "\e[32m	 ▒ ░▒░ ░ ▒   ▒▒ ░ ░░   ░ ▒░░ ▒  ▒▓██ ░▒░     ▒ ░▒░ ░ ▒   ▒▒ ░ ░  ▒  ░ ░▒ ▒░ ░▒  ░ ░\e[0m";
+	sleep 0.08
+	echo -e "\e[32m	 ░  ░░ ░ ░   ▒     ░   ░ ░ ░ ░  ░▒ ▒ ░░      ░  ░░ ░ ░   ▒  ░       ░ ░░ ░░  ░  ░  \e[0m";
+	sleep 0.08
+	echo -e "\e[32m	 ░  ░  ░     ░  ░        ░   ░   ░ ░         ░  ░  ░     ░  ░ ░     ░  ░        ░  \e[0m";
+	sleep 0.08
+	echo -e "\e[32m	                           ░     ░ ░                        ░                      \e[0m";
+	sleep 0.08
+	echo " "
+	echo -e "    	                            ====== Version 1.0 ======"
+}
+
+#===================================================================
+#			          CHECK FOR REQUIREMENTS
+#===================================================================
+Splash
+echo " "
+echo -e "HANDY HACKS v1.0 INSTALLATION..."
+echo " "
+sleep 2
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' mtr | grep "install ok installed")
+echo Checking for mtr: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "mtr not installed. Attempting to install mtr now..."
+  sleep 0.15
+  apt-get install mtr
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' dstat | grep "install ok installed")
+echo Checking for dstat: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "dstat not installed. Attempting to install dstat now..."
+  sleep 0.15
+  apt-get install dstat
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' htop | grep "install ok installed")
+echo Checking for htop: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "htop not installed. Attempting to install htop now..."
+  sleep 0.15
+  apt-get install htop
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' zenmap | grep "install ok installed")
+echo Checking for zenmap: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "zenmap not installed. Attempting to install zenmap now..."
+  sleep 0.15
+  apt-get install zenmap
+fi
+
+#===================================================================
+#			             MAIN INSTALLATION
+#===================================================================
+
+echo -e " "
+echo -e "Making Handy Hacks available globally..."
+cp hhacks.sh /bin/hhacks
+sleep 0.15
+echo -e " "
+echo -e "Copying files to correct positions..."
+chmod +x /bin/hhacks
+sleep 0.15
+echo -e " "
+echo -e "Setting proper permissions..."
+chmod +x hhacks.sh
+chmod +x officetools.sh
+sleep 0.15
+echo -e " "
+echo -e "Just type hhacks anywhere in terminal to use this utility."
+sleep 0.15
